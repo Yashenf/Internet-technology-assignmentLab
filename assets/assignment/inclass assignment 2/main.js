ca2dfd8082fa62854834455997e4d5e1197a6ac0
Yashenf <spysf2002@gmail.com>
2023-06-10 08:44:51 +0530

    finished pos

---
+++ assets/assignment/inclass assignment 2/main.js
@@ -0,0 +1,7 @@
+/*
+* Created by : @yash
+*              5/27/2023 , Saturday
+*              11:39 AM
+* Project : Assignments lab
+* Contact me : contact.yashen@gmail.com 
+*/
---
+++ assets/assignment/js-anime-inclass assignment/anime.js
@@ -0,0 +1,7 @@
+/*
+* Created by : @yash
+*              5/27/2023 , Saturday
+*              08:44 AM
+* Project : Assignments lab
+* Contact me : contact.yashen@gmail.com 
+*/
